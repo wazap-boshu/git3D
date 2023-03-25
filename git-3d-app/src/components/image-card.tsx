28b424fe545cf13f6d8a00d7b212bf561b8ec91b
イワブチフミヤ <buchi0326iwa@gmail.com>
2023-03-25 12:26:31 +0900

    ヘッダーとカードを追加
    nextjsのアプリを再構築

---
+++ git-3d-app/src/components/image-card.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+interface ImageCardProps {
+  // タイトル
+  title: string;
+  // 詳細
+  description: string;
+  // image パス
+  src: string;
+  // ボタンテキスト
+  buttonText: string;
+}
+
+export const ImageCard: FC<ImageCardProps> = (props) => {
+  const {
+    title,
+    description,
+    src,
+    buttonText,
+  } = props;
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">{buttonText}</Button>
+      </CardActions>
+    </Card>
+  )
+}
